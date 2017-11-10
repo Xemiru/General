e955788b3fc899445eec46f9ca79588324fec151
Xemiru <xemiruk@gmail.com>
2017-11-09 18:09:54 -0700

    Give javadocs a title/header.

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,12 +3,14 @@ set -ev
 
 # Build project.
 ./gradlew build
+./gradlew versionStringFile
 ls ./build/libs
 
 # Set git variables.
 export VERSION_BRANCH=$TRAVIS_BRANCH
 export VERSION_COMMIT=$TRAVIS_COMMIT
 export VERSION_TAG=$TRAVIS_TAG
+export VERSION=$(<version)
 
 # Push javadocs for release versions and snapshot versions.
 [[ "$VERSION_BRANCH" = "develop" ]] && bash ./scripts/javadocs.sh latest
--- scripts/javadocs.sh
+++ scripts/javadocs.sh
@@ -19,7 +19,7 @@ mkdir $VERSION
 cd $VERSION
 
 # Generate javadocs.
-javadoc -sourcepath $TRAVIS_BUILD_DIR/src/main/java/ -link https://docs.oracle.com/javase/8/docs/api/ -subpackages com.github.xemiru.general
+javadoc -sourcepath $TRAVIS_BUILD_DIR/src/main/java/ -link https://docs.oracle.com/javase/8/docs/api/ -subpackages com.github.xemiru.general -windowtitle "General v$VERSION Javadoc" -doctitle "General v$VERSION Javadoc"
 
 # Push to Git.
 git config --global user.name "Travis CI"
